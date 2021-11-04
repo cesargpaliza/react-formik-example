eb9e9fd16b2ca469fe8afb6f9acf6c75d2b6422e
CESAR <cesargpaliza@gmail.com>
2021-11-04 19:40:37 -0300

    se agregan manejo de formulario con formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from 'formik'
 
 function App() {
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      lastname: '',
+      email: '',    
+    },
+    validate: (values) =>{
+      const errors = {}
+      if(!values.name){
+        errors.name = 'Requerido'
+      }else if (values.name.length < 5){
+        errors.name = 'El nombre es muy corto'
+      }
+
+      if(!values.lastname){
+        errors.lastname = 'Requerido'
+      }else if (values.lastname.length < 5){
+        errors.lastname = 'El apellido es muy corto'
+      }
+
+      if(!values.email){
+        errors.email = 'Requerido'
+      }else if (values.email.length < 5){
+        errors.email = 'El email es muy corto'
+      }
+
+      return errors
+    },
+    onSubmit: values => console.log(values)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name='name'
+        tyle='text'
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.name}
+      />
+      {formik.errors.name && formik.touched.name ? formik.errors.name : null }
+      <br/>      
+      <label>Apellido</label>
+      <input
+        name='lastname'
+        tyle='text'
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastname}
+        />
+      {formik.errors.lastname && formik.touched.lastname ? formik.errors.lastname : null }
+      <br/>
+      <label>Email</label>
+      <input
+        name='email'
+        type='text'
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.errors.email && formik.touched.email ? formik.errors.email : null }
+      <br/>
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
